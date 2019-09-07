e4fccbf51e65d45c24e3d045018feec57f6dae4c
qt1280 <97007623@qq.com>
2019-09-07 17:19:58 +0800

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -5,7 +5,7 @@ yum -y install wget git unzip gcc gcc-c++ openssl-devel
 wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/caddy_install.sh && chmod +x caddy_install.sh && bash caddy_install.sh
 cd /usr/local/caddy/
 wget -N --no-check-certificate https://raw.githubusercontent.com/qt1280/aria2_perfect_config/master/caddy.conf
-nohup caddy -caddy.conf  2>&1 &
+./caddy -conf /usr/local/caddy/caddy.conf > /dev/null 2>&1 &
 cd /root/
 wget -N --no-check-certificate https://raw.githubusercontent.com/qt1280/aria2.sh/master/aria2.sh && chmod +x aria2.sh && bash aria2.sh
 
@@ -14,9 +14,9 @@ cd  /data/www/ariang
 wget https://github.com/mayswind/AriaNg-DailyBuild/archive/master.zip && unzip master.zip
 mv AriaNg-DailyBuild-master/* .
 rm -rf master.zip AriaNg-DailyBuild-master
-cd /root
-touch.rclone.html
-rclone move rclone.html joe1280:
+curl https://rclone.org/install.sh | sudo bash
+yum -y install  fuse
+rclone config
 myip=`wget http://ipecho.net/plain -O - -q echo`
 echo "网站地址是：http://${myip}"
 
